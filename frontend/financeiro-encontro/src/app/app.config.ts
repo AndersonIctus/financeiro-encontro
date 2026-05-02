469b60d4cf94486acb5de2c72b076f5f16f3aff9
Anderson Dourado <anderson.ictus@gmail.com>
2026-05-02 03:35:49 -0300

    feat: melhoria nouso dos modulos do material e inclusao do datepicker com padrão pt-br

--- frontend/financeiro-encontro/src/app/app.config.ts
+++ frontend/financeiro-encontro/src/app/app.config.ts
@@ -1,18 +1,21 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, LOCALE_ID, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePtBr from '@angular/common/locales/pt';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatIconRegistry } from '@angular/material/icon';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './general/auth/auth.interceptor';
+
+registerLocaleData(localePtBr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideAppInitializer(() => {
       inject(MatIconRegistry).setDefaultFontSetClass('material-symbols-outlined');
     }),
--- frontend/financeiro-encontro/src/app/components/login/login.component.ts
+++ frontend/financeiro-encontro/src/app/components/login/login.component.ts
@@ -1,13 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
+import { MaterialGlobalModule } from '../../shared/modules/material.imports.module';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -15,12 +10,7 @@ import { AuthService } from '../../services/auth.service';
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
+    MaterialGlobalModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
