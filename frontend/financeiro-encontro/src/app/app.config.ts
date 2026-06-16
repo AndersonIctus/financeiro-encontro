e06bddeb536f3607e97a3b677aeb2525258f6cc4
Anderson Dourado <anderson.ictus@gmail.com>
2026-06-16 20:17:12 -0300

    feat: ajustes para deploy

--- frontend/financeiro-encontro/src/app/app.config.ts
+++ frontend/financeiro-encontro/src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig, inject, LOCALE_ID, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePtBr from '@angular/common/locales/pt';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatIconRegistry } from '@angular/material/icon';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
@@ -15,8 +14,7 @@ registerLocaleData(localePtBr);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideAnimationsAsync(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([authInterceptor])),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideAppInitializer(() => {
